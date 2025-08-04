bf0fcc33eecc799ae92f78192fcddf118f0fb45f
Xiomaraht <xiomy6789@gmail.com>
2025-08-04 17:09:24 -0500

    wdeq

--- src/main/java/com/sena/clase1/Inicio.java
+++ src/main/java/com/sena/clase1/Inicio.java
@@ -5,13 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 
+
     @RestController
 public class Inicio {
 
     @GetMapping("/")
-    public String getMethodName() {
+    public String getMethodName1() {
         return new String("Hola mundo, bienvenido a mi primer proyecto de springboot");
     }
+
+    @GetMapping("/Inicio")
+    public String getMethodName2() {
+        return new String("Hola mundo, bienvenido a mi segundo getMapping");
+    }
+    
 
 }
 
