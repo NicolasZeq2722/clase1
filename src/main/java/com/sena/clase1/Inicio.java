f04a148de20e2fdc550b7f9e4b797395a8cdb502
Xiomaraht <xiomy6789@gmail.com>
2025-08-04 17:33:54 -0500

    Nuevo

--- src/main/java/com/sena/clase1/Inicio.java
+++ src/main/java/com/sena/clase1/Inicio.java
@@ -1,26 +1,39 @@
 package com.sena.clase1;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
     @RestController
 public class Inicio {
 
     @GetMapping("/")
-    public String getMethodName1() {
-        return new String("Hola mundo, bienvenido a mi primer proyecto de springboot");
+    public String metodoGet() {
+        return new String("Hola mundo, bienvenido a GetMapping");
     }
 
-    @GetMapping("/Inicio")
-    public String getMethodName2() {
-        return new String("Hola mundo, bienvenido a mi segundo getMapping");
+    @PostMapping("/Post")
+    public String metodoPost() {
+        return new String("PostMapping");
     }
-    
+
+    @PutMapping("/Put")
+    public String metodoPut() {
+        return new String("PutMapping");
+    }
+
+    @DeleteMapping("/Delete")
+    public String metodoDelete() {
+        return new String("DeleteMapping");
+    }
+
+    @PatchMapping("/Path")
+    public String metodoPatch() {
+        return new String("PatchMapping");
+    }
 
 }
-
-
-    
